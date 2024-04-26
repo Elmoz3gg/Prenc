1a5eaf69d1c9c32ff2d2a85182040236a768468b
Elmoz3gg <162893646+Elmoz3gg@users.noreply.github.com>
2024-04-26 09:02:20 +0300

    Delete plugins directory

--- plugins/owner-unbanchat.js
+++
@@ -1,9 +0,0 @@
-const handler = async (m) => {
-  global.db.data.chats[m.chat].isBanned = false;
-  m.reply('*[❗] تم إلغاء حظر المجموعة بنجاح*');
-};
-handler.help = ['إلغاءحظرالمجموعة'];
-handler.tags = ['owner'];
-handler.command = /^إلغاء-حظر-المجموعة$/i;
-handler.rowner = true;
-export default handler;
